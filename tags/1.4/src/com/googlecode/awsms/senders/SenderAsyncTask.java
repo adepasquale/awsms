2bbbb64f6e9bb8c75eafacc64d0517499df44cac
andrea@de-pasquale.name <andrea@de-pasquale.name@49fac842-718e-3139-d292-07df5665563c>
2010-11-29 21:59:02 +0000

    Tagging Android Web SMS 1.4

---
+++ tags/1.4/src/com/googlecode/awsms/senders/SenderAsyncTask.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2010 Andrea De Pasquale
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.awsms.senders;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.googlecode.awsms.AndroidWebSMS;
+import com.googlecode.awsms.R;
+
+/**
+ * Asynchronous task which sends an SMS while displaying a progress dialog.
+ * 
+ * @author Andrea De Pasquale
+ */
+public class SenderAsyncTask extends AsyncTask<Void, byte[], Integer> {
+// TODO make this a background service with an outgoing message queue
+
+	private AndroidWebSMS androidWebSMS;
+	private ProgressDialog progressDialog;
+
+	public SenderAsyncTask() {
+		androidWebSMS = AndroidWebSMS.getApplication();
+		progressDialog = null;
+	}
+
+	@Override
+	protected void onPreExecute() {
+		progressDialog = ProgressDialog.show(androidWebSMS.getComposeActivity(),
+				androidWebSMS.getString(R.string.ProgressDialogTitle), 
+				androidWebSMS.getString(R.string.ProgressDialogMessage), 
+				true, true);
+		progressDialog.setOnCancelListener(new OnCancelListener() {
+			public void onCancel(DialogInterface dialog) {
+				SenderAsyncTask.this.cancel(true);
+			}
+		});
+	}
+	
+	@Override
+	protected Integer doInBackground(Void... params) {
+		WebSender webSender = androidWebSMS.getWebSender();
+		String username = androidWebSMS.getUsername();
+		String password = androidWebSMS.getPassword();
+		String receiver = androidWebSMS.getReceiver();
+		String message = androidWebSMS.getMessage();
+		String captcha = androidWebSMS.getCaptcha();
+
+		switch (webSender.send(username, password, receiver, message, captcha)) {
+		case SETTINGS_INVALID: 
+			return R.string.WebSenderSettingsInvalid;
+		case RECEIVER_INVALID:
+			return R.string.WebSenderReceiverInvalid;
+		case MESSAGE_INVALID:
+			return R.string.WebSenderMessageInvalid;
+		case WEBSITE_UNAVAILABLE:
+			return R.string.WebSenderWebsiteUnavailable;
+		case OUT_OF_MESSAGES:
+			return R.string.WebSenderOutOfMessages;
+		case NEED_CAPTCHA:
+			publishProgress(webSender.getCaptchaArray());
+			return R.string.WebSenderNeedCaptcha;
+		case MESSAGE_SENT:
+			publishProgress();
+			return R.string.WebSenderMessageSent;
+		default:
+			return R.string.WebSenderUnknownError;
+		}
+	}
+	
+	@Override
+    protected void onProgressUpdate(byte[]... progress) {
+        if (progress.length > 0) {
+			androidWebSMS.showCaptchaLayout(progress[0]);
+        } else {
+        	androidWebSMS.hideCaptchaLayout();
+        	androidWebSMS.saveWebSMS();
+        	androidWebSMS.resetEditText();
+        }
+    }
+	
+	@Override
+	protected void onPostExecute(Integer result) {
+		if (progressDialog != null) {
+			progressDialog.dismiss();
+		}
+		Toast.makeText(androidWebSMS.getComposeActivity(), 
+				result, Toast.LENGTH_LONG).show();
+	}
+	
+	@Override
+	protected void onCancelled() {
+		Toast.makeText(androidWebSMS.getComposeActivity(), 
+				R.string.ProgressDialogCanceled, 
+				Toast.LENGTH_LONG).show();
+	}
+	
+}
---
+++ tags/1.4/src/com/googlecode/awsms/ui/ComposeActivity.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2010 Andrea De Pasquale
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.awsms.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.googlecode.awsms.AndroidWebSMS;
+import com.googlecode.awsms.R;
+import com.googlecode.awsms.db.ReceiverAdapter;
+
+/**
+ * Activity used to compose a message and send it by pressing a button. 
+ * If needed, also displays an additional view to manually decode a CAPTCHA.
+ * 
+ * @author Andrea De Pasquale
+ */
+public class ComposeActivity extends Activity {
+	
+	private AndroidWebSMS androidWebSMS;
+	
+ 	private AutoCompleteTextView receiverText;
+	private EditText messageText;
+	private TextView messageCounter;
+	private Button messageSend;
+	private LinearLayout captchaLayout;
+	private ImageView captchaImage;
+	private EditText captchaText;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		receiverText = (AutoCompleteTextView) findViewById(R.id.ReceiverText);
+		messageText = (EditText) findViewById(R.id.MessageText);
+		messageCounter = (TextView) findViewById(R.id.MessageCounter);
+		messageSend = (Button) findViewById(R.id.MessageSend);
+		captchaLayout = (LinearLayout) findViewById(R.id.Captcha);
+		captchaImage = (ImageView) findViewById(R.id.CaptchaImage);
+		captchaText = (EditText) findViewById(R.id.CaptchaText);
+		// TODO sent messages counter
+		
+		androidWebSMS = AndroidWebSMS.getApplication();
+		androidWebSMS.setComposeActivity(this);
+		
+ 		receiverText.setAdapter(new ReceiverAdapter(this));
+
+        messageText.addTextChangedListener(new TextWatcher() {
+			public void beforeTextChanged(
+					CharSequence s, int start, int count, int after) {}
+			public void afterTextChanged(Editable s) {}
+        	public void onTextChanged(
+        			CharSequence s, int start, int before, int count) {
+        		androidWebSMS.updateMessageCounter(s.length());
+			}
+		});
+		
+        androidWebSMS.updateMessageCounter(0);
+		
+		messageSend.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				androidWebSMS.doSendWebSMS();
+			}
+		});
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.layout.menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.ClearMenuItem:
+            receiverText.setText("");
+            messageText.setText("");
+            captchaText.setText("");
+    		captchaLayout.setVisibility(View.GONE);
+	        return true;
+	    case R.id.SettingsMenuItem:
+            startActivity(new Intent(this, SettingsActivity.class));
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		androidWebSMS.cancelSendWebSMS();
+	}
+
+	public AutoCompleteTextView getReceiverText() {
+		return receiverText;
+	}
+
+	public EditText getMessageText() {
+		return messageText;
+	}
+
+	public TextView getMessageCounter() {
+		return messageCounter;
+	}
+
+	public Button getMessageSend() {
+		return messageSend;
+	}
+
+	public LinearLayout getCaptchaLayout() {
+		return captchaLayout;
+	}
+
+	public ImageView getCaptchaImage() {
+		return captchaImage;
+	}
+
+	public EditText getCaptchaText() {
+		return captchaText;
+	}
+
+}
