62619160132578f0f70f1f75f2b224cad0f402a6
andrea@de-pasquale.name <andrea@de-pasquale.name@49fac842-718e-3139-d292-07df5665563c>
2010-12-03 23:22:20 +0000

    Finally solved orientation change bug with AsyncTask. Need to add message counter dialog in options menu.

--- trunk/gen/com/googlecode/awsms/R.java
+++ trunk/gen/com/googlecode/awsms/R.java
@@ -19,16 +19,19 @@ public final class R {
         public static final int Captcha=0x7f050006;
         public static final int CaptchaImage=0x7f050007;
         public static final int CaptchaText=0x7f050008;
-        public static final int ClearMenuItem=0x7f050009;
+        public static final int ClearMenuItem=0x7f05000c;
+        public static final int Information=0x7f050009;
+        public static final int InformationName=0x7f05000a;
         public static final int Message=0x7f050001;
         public static final int MessageCounter=0x7f050004;
         public static final int MessageSend=0x7f050005;
         public static final int MessageText=0x7f050002;
         public static final int MessageTools=0x7f050003;
-        public static final int ReceiverName=0x7f05000b;
-        public static final int ReceiverNumber=0x7f05000c;
+        public static final int ReceiverName=0x7f05000e;
+        public static final int ReceiverNumber=0x7f05000f;
         public static final int ReceiverText=0x7f050000;
-        public static final int SettingsMenuItem=0x7f05000a;
+        public static final int SettingsMenuItem=0x7f05000d;
+        public static final int TextView02=0x7f05000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -38,30 +41,31 @@ public final class R {
     }
     public static final class string {
         public static final int CaptchaTextHint=0x7f040005;
-        public static final int ClearMenuItem=0x7f040006;
+        public static final int ClearMenuItem=0x7f040007;
         public static final int ComposeActivityLabel=0x7f040001;
+        public static final int InformationName=0x7f040006;
         public static final int MessageSend=0x7f040004;
         public static final int MessageTextHint=0x7f040003;
-        public static final int ProgressDialogCanceled=0x7f040011;
-        public static final int ProgressDialogMessage=0x7f040010;
-        public static final int ProgressDialogTitle=0x7f04000f;
+        public static final int ProgressDialogCanceled=0x7f040012;
+        public static final int ProgressDialogMessage=0x7f040011;
+        public static final int ProgressDialogTitle=0x7f040010;
         public static final int ReceiverTextHint=0x7f040002;
-        public static final int SaveWebSMSPreference=0x7f040009;
-        public static final int SaveWebSMSPreferenceOff=0x7f04000b;
-        public static final int SaveWebSMSPreferenceOn=0x7f04000a;
-        public static final int SettingsActivityLabel=0x7f040008;
-        public static final int SettingsMenuItem=0x7f040007;
-        public static final int VodafoneItalyPasswordPreference=0x7f04000e;
-        public static final int VodafoneItalyPreferenceCategory=0x7f04000c;
-        public static final int VodafoneItalyUsernamePreference=0x7f04000d;
-        public static final int WebSenderMessageInvalid=0x7f040014;
-        public static final int WebSenderMessageSent=0x7f040019;
-        public static final int WebSenderNeedCaptcha=0x7f040017;
-        public static final int WebSenderOutOfMessages=0x7f040016;
-        public static final int WebSenderReceiverInvalid=0x7f040013;
-        public static final int WebSenderSettingsInvalid=0x7f040012;
-        public static final int WebSenderUnknownError=0x7f040018;
-        public static final int WebSenderWebsiteUnavailable=0x7f040015;
+        public static final int SaveWebSMSPreference=0x7f04000a;
+        public static final int SaveWebSMSPreferenceOff=0x7f04000c;
+        public static final int SaveWebSMSPreferenceOn=0x7f04000b;
+        public static final int SettingsActivityLabel=0x7f040009;
+        public static final int SettingsMenuItem=0x7f040008;
+        public static final int VodafoneItalyPasswordPreference=0x7f04000f;
+        public static final int VodafoneItalyPreferenceCategory=0x7f04000d;
+        public static final int VodafoneItalyUsernamePreference=0x7f04000e;
+        public static final int WebSenderMessageInvalid=0x7f040015;
+        public static final int WebSenderMessageSent=0x7f04001a;
+        public static final int WebSenderNeedCaptcha=0x7f040018;
+        public static final int WebSenderOutOfMessages=0x7f040017;
+        public static final int WebSenderReceiverInvalid=0x7f040014;
+        public static final int WebSenderSettingsInvalid=0x7f040013;
+        public static final int WebSenderUnknownError=0x7f040019;
+        public static final int WebSenderWebsiteUnavailable=0x7f040016;
         public static final int app_name=0x7f040000;
     }
 }
