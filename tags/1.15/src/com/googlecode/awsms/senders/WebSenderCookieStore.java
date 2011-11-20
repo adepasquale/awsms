8cbf986d5c7cd1e67d4c7a55ef13671a64f904cd
andrea@de-pasquale.name <andrea@de-pasquale.name@49fac842-718e-3139-d292-07df5665563c>
2011-11-20 16:52:19 +0000

    Tagging awsms-1.15

---
+++ tags/1.15/src/com/googlecode/awsms/senders/WebSenderCookieStore.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2010-2011 Andrea De Pasquale
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.awsms.senders;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.http.client.CookieStore;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.cookie.BasicClientCookie;
+
+// TODO implement CookieStore without using BasicCookieStore
+public class WebSenderCookieStore implements CookieStore, Serializable {
+  static final long serialVersionUID = 1L;
+
+  transient CookieStore cookieStore;
+
+  public WebSenderCookieStore() {
+    cookieStore = new BasicCookieStore();
+  }
+
+  @Override
+  public void addCookie(Cookie cookie) {
+    cookieStore.addCookie(cookie);
+  }
+
+  @Override
+  public void clear() {
+    cookieStore.clear();
+  }
+
+  @Override
+  public boolean clearExpired(Date date) {
+    return cookieStore.clearExpired(date);
+  }
+
+  @Override
+  public List<Cookie> getCookies() {
+    return cookieStore.getCookies();
+  }
+
+  private void readObject(ObjectInputStream input) throws IOException,
+      ClassNotFoundException {
+    cookieStore = new BasicCookieStore();
+
+    input.defaultReadObject();
+    int size = input.readInt();
+    while (size-- > 0) {
+      WebSenderCookie wsCookie = (WebSenderCookie) input.readObject();
+      cookieStore.addCookie(wsCookie.getCookie());
+    }
+  }
+
+  private void writeObject(ObjectOutputStream output) throws IOException {
+    output.defaultWriteObject();
+    List<Cookie> cookies = cookieStore.getCookies();
+    output.writeInt(cookies.size());
+    for (Cookie cookie : cookies) {
+      output.writeObject(new WebSenderCookie(cookie));
+    }
+  }
+
+  // TODO implement Cookie interface entirely
+  class WebSenderCookie implements Serializable {
+    static final long serialVersionUID = 1L;
+
+    int version;
+    String name;
+    String value;
+    String domain;
+    String path;
+    long expiry;
+
+    public WebSenderCookie(Cookie cookie) {
+      version = cookie.getVersion();
+      name = cookie.getName();
+      value = cookie.getValue();
+      domain = cookie.getDomain();
+      path = cookie.getPath();
+
+      expiry = 0;
+      Date expiryDate = cookie.getExpiryDate();
+      if (expiryDate != null)
+        expiry = expiryDate.getTime();
+    }
+
+    public Cookie getCookie() {
+      BasicClientCookie cookie = new BasicClientCookie(name, value);
+      cookie.setVersion(version);
+      cookie.setDomain(domain);
+      cookie.setPath(path);
+      if (expiry != 0)
+        cookie.setExpiryDate(new Date(expiry));
+      return cookie;
+    }
+  }
+}
---
+++ tags/1.15/src/com/googlecode/awsms/senders/WebSenderHttpClient.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2010-2011 Andrea De Pasquale
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.awsms.senders;
+
+import java.io.InputStream;
+import java.security.KeyStore;
+
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+
+import android.content.Context;
+
+import com.googlecode.awsms.R;
+
+public class WebSenderHttpClient extends DefaultHttpClient {
+
+  final Context context;
+
+  public WebSenderHttpClient(Context context) {
+    this.context = context;
+  }
+
+  @Override
+  protected ClientConnectionManager createClientConnectionManager() {
+    SchemeRegistry registry = new SchemeRegistry();
+    registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(),
+        80));
+    // Register for port 443 our SSLSocketFactory
+    // with our keystore to the ConnectionManager
+    registry.register(new Scheme("https", newSslSocketFactory(), 443));
+    return new SingleClientConnManager(getParams(), registry);
+  }
+
+  private SSLSocketFactory newSslSocketFactory() {
+    try {
+      // Get an instance of the Bouncy Castle KeyStore format
+      KeyStore trusted = KeyStore.getInstance("BKS");
+      // Get the raw resource, which contains the keystore with
+      // your trusted certificates (root and any intermediate certs)
+      InputStream in = context.getResources().openRawResource(R.raw.keystore);
+
+      try {
+        // Initialize the keystore with the provided trusted
+        // certificates. Also provide the password of the keystore
+        trusted.load(in, "storepass".toCharArray());
+      } finally {
+        in.close();
+      }
+
+      // Pass the keystore to the SSLSocketFactory. The factory is
+      // responsible for the verification of the server certificate.
+      SSLSocketFactory sf = new SSLSocketFactory(trusted);
+      // Hostname verification from certificate
+      // http://hc.apache.org/httpcomponents-client-ga/tutorial/html/connmgmt.html#d4e506
+      sf.setHostnameVerifier(SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
+      return sf;
+
+    } catch (Exception e) {
+      throw new AssertionError(e);
+    }
+  }
+}
